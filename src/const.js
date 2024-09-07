c315b43ba6d6e091ab751c9a08c02019cd797d88
VslavV <um.w@mail.ru>
2024-09-07 14:46:59 +0500

    добавляет модель

--- src/const.js
+++ src/const.js
@@ -2,7 +2,7 @@ const FILTER_TYPES = ['everything', 'future', 'present', 'past'];
 const SORT_TYPES = ['day', 'event', 'time', 'price', 'offers'];
 const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const DESTINATION_LOCATIONS = ['Amsterdam', 'Geneva', 'Chamonix', 'Moscow', 'Madrid'];
-const NUMBER_LOCATION_POINTS = 3;
+const NUMBER_LOCATION_POINTS = 5;
 const OFFER_TITLE = ['Order Uber', 'Add luggage', 'Switch to comfort', 'Rent a car', 'Add breakfast', 'Book tickets', 'Lunch in city'];
 const OFFER_SELECTORS = [
   { id: 'luggage-1', checkbox: true, title: 'Add luggage', price: 30 },
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,4 +1,3 @@
-import { NUMBER_LOCATION_POINTS } from '../const.js';
 import FormCreateView from '../view/form-create-view.js';
 import FormEditView from '../view/form-edit-view.js';
 import LocationPointView from '../view/location-point-view.js';
@@ -7,16 +6,20 @@ import SortView from '../view/sort-view';
 import { render, RenderPosition } from '../render.js';
 
 export default class MainPresenter {
-  constructor({ container }) {
+  constructor({ container, eventPointModel }) {
     this.container = container;
+    this.eventPointModel = eventPointModel;
   }
 
   init() {
+    this.eventsListPoints = [...this.eventPointModel.getEventPoint()];
+
     render(new SortView(), this.container, RenderPosition.BEFOREEND);
     render(new FormEditView(), this.container, RenderPosition.BEFOREEND);
     render(new FormCreateView(), this.container, RenderPosition.BEFOREEND);
-    for (let i = 0; i < NUMBER_LOCATION_POINTS; i++) {
-      render(new LocationPointView(), this.container, RenderPosition.BEFOREEND);
+
+    for (let i = 0; i < this.eventsListPoints.length; i++) {
+      render(new LocationPointView({ eventPoint: this.eventsListPoints[i] }), this.container, RenderPosition.BEFOREEND);
     }
   }
 }
