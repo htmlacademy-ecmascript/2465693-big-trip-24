b1c3f94cbf207e6f7480c85a42d7b5fcf239ad4c
VslavV <um.w@mail.ru>
2024-09-28 14:32:32 +0500

    исправляет отображение выбранного типа в списке

--- src/view/form-edit-view.js
+++ src/view/form-edit-view.js
@@ -14,9 +14,9 @@ const createOfferItemTemplate = (type, title, price, className) => `
   </div>
 `;
 
-const createTypeItemTemplate = (type) => `
+const createTypeItemTemplate = (type, isCheckedTypeClassName) => `
   <div class="event__type-item">
-    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
+    <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${isCheckedTypeClassName}>
     <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${capitalizeLetter(type)}</label>
   </div>
 `;
@@ -54,7 +54,7 @@ const createNewFormEditViewTemplate = ({ eventPoint, offers, destinations }) => 
 
   const createAvailableTypes = typeOffers
     .map((item) => {
-      const isCheckedTypeClassName = item === capitalizeLetter(type) ? 'checked' : '';
+      const isCheckedTypeClassName = item === type ? 'checked' : '';
       return createTypeItemTemplate(item, isCheckedTypeClassName);
     })
     .join('');
