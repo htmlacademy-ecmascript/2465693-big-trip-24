ce7500909661a7e9e19792f8c4be5a8643dd5e13
VslavV <um.w@mail.ru>
2024-10-16 20:01:22 +0500

    устранение замечаний при проверке

--- src/presenter/new-event-presenter.js
+++ src/presenter/new-event-presenter.js
@@ -65,7 +65,6 @@ export default class NewEventPresenter {
       this.#addComponent.updateElement({
         isDisabled: false,
         isSaving: false,
-        isDeleting: false,
       });
     };
 
